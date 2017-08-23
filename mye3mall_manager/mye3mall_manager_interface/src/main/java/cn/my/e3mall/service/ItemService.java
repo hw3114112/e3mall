c98c530b78f2631c3f5620816438db87096344f8
hw311 <hw311@RX-00>
2017-08-24 00:46:58 +0800

    电商项目 聚合工程

---
+++ mye3mall_manager/mye3mall_manager_interface/src/main/java/cn/my/e3mall/service/ItemService.java
@@ -0,0 +1,9 @@
+package cn.my.e3mall.service;
+
+import cn.my.e3mall.pojo.TbItem;
+
+public interface ItemService {
+
+	TbItem getItemById(long id);
+
+}
---
+++ mye3mall_manager/mye3mall_manager_service/src/main/java/cn/my/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.my.e3mall.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.my.e3mall.mapper.TbItemMapper;
+import cn.my.e3mall.pojo.TbItem;
+import cn.my.e3mall.service.ItemService;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Override
+	public TbItem getItemById(long id) {
+		TbItem item = itemMapper.selectByPrimaryKey(id);
+		return item;
+	}
+
+}
